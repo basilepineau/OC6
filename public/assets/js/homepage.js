86d8d44c0922231b3dfb73097dc6326632fc4c23
Basile <basile.pineau64@orange.fr>
2024-07-29 16:28:07 +0200

    feat(loadmore): implement loadmore button on trick details page

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -38,48 +38,4 @@ document.addEventListener("DOMContentLoaded", () => {
       loadMoreButton.innerHTML = "All tricks are loaded.";
     }
   });
-
-  // const cardContainer = document.getElementById("card-container");
-  // const loadMoreButton = document.getElementById("load-more");
-
-  // const cardLimit = 61;
-  // const cardIncrease = 5;
-  // const pageCount = Math.ceil(cardLimit / cardIncrease);
-
-  // let currentPage = 1;
-
-  // const createCard = (index) => {
-  //   const card = document.createElement("div");
-  //   card.className = "card";
-  //   card.innerHTML = index;
-  //   cardContainer.appendChild(card);
-  // };
-
-  // const addCards = (pageIndex) => {
-  //   currentPage = pageIndex;
-
-  //   handleButtonStatus();
-
-  //   const startRange = (pageIndex - 1) * cardIncrease;
-  //   const endRange =
-  //     currentPage == pageCount ? cardLimit : pageIndex * cardIncrease;
-
-  //   for (let i = startRange + 1; i <= endRange; i++) {
-  //     createCard(i);
-  //   }
-  // };
-
-  // const handleButtonStatus = () => {
-  //   if (pageCount === currentPage) {
-  //     loadMoreButton.classList.add("disabled");
-  //     loadMoreButton.setAttribute("disabled", true);
-  //   }
-  // };
-
-  // window.onload = function () {
-  //   addCards(currentPage);
-  //   loadMoreButton.addEventListener("click", () => {
-  //     addCards(currentPage + 1);
-  //   });
-  // };
 });
